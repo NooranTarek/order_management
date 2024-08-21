b535aa2ad26f0f3f1f9f9f7bfc3857de67e62560
NooranTarek <noort.mohamed@gmail.com>
2024-08-21 22:53:35 +0300

    set initial routes/controllers/models for categories / products / orders

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
-
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 //________________user routes____________________
 Route::apiResource('users', UserController::class);
 
+//________________order routes____________________
+Route::apiResource('orders', OrderController::class);
+
+//________________category routes____________________
+Route::apiResource('categories', CategoryController::class);
+
+//________________product routes____________________
+Route::apiResource('products', ProductController::class);
