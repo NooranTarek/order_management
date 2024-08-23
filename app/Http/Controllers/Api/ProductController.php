bee6805662f4f034873dd8357fd8cf78ff79c042
NooranTarek <noort.mohamed@gmail.com>
2024-08-23 19:41:18 +0300

    edit get specific product data

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -67,7 +67,7 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        $product=Product::find($id);
+        $product=Product::with('category')->find($id);
         if (!$product){
             return response()->json(['message'=>'Product not found'],404);
         }
